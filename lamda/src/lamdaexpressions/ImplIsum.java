b139254812d370aa38e0a2470151de44e7e27b20
hp <hp@DESKTOP-9MT34QV>
2021-04-08 11:32:13 +0530

    lamda

---
+++ lamda/src/lamdaexpressions/ImplIsum.java
@@ -0,0 +1,9 @@
+package lamdaexpressions;
+
+public class ImplIsum implements Isum{
+	public int add(int a , int b)
+	{
+		return a+b;
+	}
+
+}
---
+++ lamda/src/lamdaexpressions/lifeB4FunctionalInterface.java
@@ -0,0 +1,9 @@
+package lamdaexpressions;
+
+public class lifeB4FunctionalInterface {
+	public static void main(String[] args) {
+		ImplIsum isum = new ImplIsum();
+		System.out.println(isum.add(10,20));
+	}
+
+}
