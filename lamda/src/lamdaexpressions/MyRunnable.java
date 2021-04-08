b518bdb612eacf73c31d19ec7a6b4df1cd6f0bf7
hp <hp@DESKTOP-9MT34QV>
2021-04-08 12:00:31 +0530

    commit

--- lamda/src/lamdaexpressions/MyRunnable.java
+++ lamda/src/lamdaexpressions/MyRunnable.java
@@ -3,8 +3,12 @@ package lamdaexpressions;
 public class MyRunnable implements Runnable {
 	public void run()
 	{
+		for(int i=0;i<5;i++)
+		{
 		System.out.println("mainThread");
 	}
+	}
+}
 	
 
-}
+
--- lamda/src/lamdaexpressions/ThreadTest.java
+++ lamda/src/lamdaexpressions/ThreadTest.java
@@ -3,9 +3,18 @@ package lamdaexpressions;
 public class ThreadTest {
 	public static void main(String[] args)
 	{
-		MyRunnable r =new MyRunnable();
+		Runnable r =() -> {
+			for(int i=0;i<5;i++)
+			{
+				System.out.println("child thread");
+			}
+		};
 		Thread t =new Thread(r);
 		t.start();
+		for(int i=0;i<5;i++)
+		{
+			System.out.println("MianThread");
+		}
 	}
 
 }
