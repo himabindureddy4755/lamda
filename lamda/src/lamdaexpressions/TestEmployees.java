9674a263b67acc33c348074e091bbd50ac03a972
hp <hp@DESKTOP-9MT34QV>
2021-04-08 14:43:30 +0530

    employeename comparator

--- lamda/src/lamdaexpressions/TestEmployees.java
+++ lamda/src/lamdaexpressions/TestEmployees.java
@@ -13,8 +13,10 @@ public class TestEmployees {
 		employees.add(new Employee("div",425));
 		employees.add(new Employee("harsh",439));
 		System.out.println(employees);
-		Comparator<Employee> c =(e1, e2) -> (e1.eid < e2.eid)?-1:(e1.eid > e2.eid)?1:0;
-		Collections.sort(employees,c);
+		//Comparator<Employee> c =(e1, e2) -> (e1.eid < e2.eid)?-1:(e1.eid > e2.eid)?1:0;
+		Comparator<Employee> n =(e1, e2)-> (e1.name.compareTo(e2.name));
+		Collections.sort(employees,n);
+		//Collections.sort(employees,c);
 		System.out.println(employees);
 	}
 
