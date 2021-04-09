663131eb07d5ad29c8d2d5e3165051e954d1e1a4
hp <hp@DESKTOP-9MT34QV>
2021-04-09 10:14:00 +0530

    static

---
+++ lamda/src/Lamdaexpressionsanonymousdefaultstatic/Iutility.java
@@ -0,0 +1,9 @@
+package Lamdaexpressionsanonymousdefaultstatic;
+
+public interface Iutility {
+	public static boolean isEligible(int age) {
+		return (age>=18)? true : false;
+		
+	}
+
+}
--- lamda/src/Lamdaexpressionsanonymousdefaultstatic/TestDefault.java
+++ lamda/src/Lamdaexpressionsanonymousdefaultstatic/TestDefault.java
@@ -3,8 +3,9 @@ package Lamdaexpressionsanonymousdefaultstatic;
 public class TestDefault {
 	public static void main(String[] args)
 	{
-		FirstImpl f = new FirstImpl();
-		f.method3();
+		System.out.println(Iutility.isEligible(17));
+		//FirstImpl f = new FirstImpl();
+		//f.method3();
 	}
 
 }
