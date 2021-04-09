1f5c79b3f8f7f07521323e3fa027509f4740ec4a
hp <hp@DESKTOP-9MT34QV>
2021-04-09 12:54:08 +0530

    commit

--- lamda/src/predicates/functions/ConsumerTest.java
+++ lamda/src/predicates/functions/ConsumerTest.java
@@ -2,17 +2,22 @@ package predicates.functions;
 
 import java.util.ArrayList;
 import java.util.function.Consumer;
+import java.util.function.Predicate;
 
 public class ConsumerTest {
 		static ArrayList<JavaStudent> javaStudents = StudentTest.populateStudents();
 		public static void main(String[] args)
 		{
+			Predicate<JavaStudent >p =s ->s.marks>=60;
 			Consumer<JavaStudent> c =s->System.out.println(s.rollno);
+			System.out.println("students who have got 60 and above");
 			for(JavaStudent student:javaStudents)
 			{
+				if(p.test(student)) {
 				c.accept(student);
 			}
 		}
+}
 }
 
 
