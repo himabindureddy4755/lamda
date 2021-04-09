79b81c4ec5ddd0e3e2116c2fc114751c7e2d7cf3
hp <hp@DESKTOP-9MT34QV>
2021-04-09 11:49:49 +0530

    chain predicates

---
+++ lamda/src/predicates/ChainPredicates.java
@@ -0,0 +1,26 @@
+package predicates;
+
+import java.util.function.Predicate;
+
+public class ChainPredicates {
+	public static void main(String[] args) {
+		Predicate<Integer> p1 =i->i%3==0;//check for divisible by 3;
+		Predicate<Integer> p2 =i->i%4==0;//check for divisible by 4
+		int[] intArray ={10,15,16,24,48,36,60};
+		for (int i :intArray)
+		{
+			if(p1.and(p2).test(i))
+			{
+				System.out.println(i);
+				System.out.println(":divisible by 12");
+				
+			}
+			else {
+				System.out.println(i);
+				System.out.println(":not divisible by 12");
+			}
+		}
+		
+
+}
+	}
