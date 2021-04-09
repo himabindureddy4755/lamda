7828d1b1b33106894b9633b15924a7637cd489a5
hp <hp@DESKTOP-9MT34QV>
2021-04-09 10:28:17 +0530

    predicate

---
+++ lamda/src/Inbuiltfunctionalinterface/TestPredicate.java
@@ -0,0 +1,12 @@
+package Inbuiltfunctionalinterface;
+
+import java.util.function.Predicate;
+
+public class TestPredicate {
+	public static void main(String[] args)
+	{
+		Predicate<Integer> p = i ->i%2 ==0;
+		System.out.println(p.test(13));
+		}
+
+}
