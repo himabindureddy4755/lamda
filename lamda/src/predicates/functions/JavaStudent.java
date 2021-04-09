948fce0b1b3d5abdcb739c279238ba4725b6825c
hp <hp@DESKTOP-9MT34QV>
2021-04-09 12:41:56 +0530

    students

---
+++ lamda/src/predicates/functions/JavaStudent.java
@@ -0,0 +1,12 @@
+package predicates.functions;
+
+public class JavaStudent {
+	public JavaStudent(int rollno, int marks) {
+		super();
+		this.rollno = rollno;
+		this.marks = marks;
+	}
+	public int rollno;
+	int marks;
+
+}
---
+++ lamda/src/predicates/functions/StudentTest.java
@@ -0,0 +1,36 @@
+package predicates.functions;
+
+import java.util.ArrayList;
+import java.util.function.Function;
+
+public class StudentTest {
+
+	static ArrayList<JavaStudent> javaStudents;
+	public static void main(String[] args)
+	{
+		populateStudents();
+		Function<JavaStudent,String> f =(j) ->{
+			if(j.marks>80) return "distinction";
+			else if(j.marks>60) return "first class";
+			return "fail";
+				
+		};
+	
+		for(JavaStudent s: javaStudents)
+		{
+			System.out.println(s.rollno+"\t"+f.apply(s));
+		}
+		
+		
+		
+	}
+	public static void populateStudents() {
+		javaStudents = new ArrayList<JavaStudent>();
+		javaStudents.add(new JavaStudent(442,90));
+		javaStudents.add(new JavaStudent(439,50));
+		javaStudents.add(new JavaStudent(425,60));
+		// TODO Auto-generated method stub
+		
+	}
+
+}
